dfc135eb5a8a11b79e1c07d604fec99ac07b4603
thong hoc lap trinh <lethong0703@gmail.com>
2026-03-19 17:37:50 +0700

    update

--- bai6.java
+++ bai6.java
@@ -1,5 +1,7 @@
 package Tryhard;
 
+import java.util.Scanner;
+
 public class bai6 {
 //	Bài 6 (Level khó)
 //
@@ -12,4 +14,43 @@ public class bai6 {
 //	Nếu nghiệm kép → in nghiệm
 //
 //	Nếu vô nghiệm → thông báo
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập a: ");
+		double a = sc.nextDouble();
+
+		System.out.print("Nhập b: ");
+		double b = sc.nextDouble();
+
+		System.out.print("Nhập c: ");
+		double c = sc.nextDouble();
+
+		if (a == 0) {
+			System.out.println("Không phải phương trình bậc 2");
+		} else {
+
+			double delta = b * b - 4 * a * c;
+
+			if (delta < 0) {
+				System.out.println("No solution");
+			} else if (delta == 0) {
+
+				double x = -b / (2 * a);
+				System.out.println("Có nghiệm kép: " + x);
+
+			} else {
+
+				double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+				double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+				if (x1 > x2) {
+					System.out.println("nghiệm lớn hơn là: " + x1);
+				} else {
+					System.out.println("nghiệm lớn hơn là: " + x2);
+				}
+			}
+		}
+
+		sc.close();
+	}
 }
